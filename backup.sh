1ee857ac7decb259ca5a10263ea2331162935f20
Kristián Feldsam <feldsam@gmail.com>
2017-08-27 23:31:54 +0200

    Fixed backup script
    Signed-off-by: Kristián Feldsam <feldsam@gmail.com>

--- backup.sh
+++ backup.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
+
+BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 echo "Backup images"
-one-image-backup.js -vs
+$BASEDIR/one-image-backup.js -vs
 
 echo "Backup deployments - system datastores without non-persistent disks"
 rsync -avhP --delete --exclude "disk.0.snap/0" oneadmin@node1.feldhost.cz:/var/lib/one/datastores/0/ /var/data/opennebula/0/
