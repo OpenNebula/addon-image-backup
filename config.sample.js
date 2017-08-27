58b33058827d00f1b9af2a61aaee28e2a8831430
Kristián Feldsam <feldsam@gmail.com>
2017-08-27 14:52:53 +0200

    Added bridgelist to sample config
    Signed-off-by: Kristián Feldsam <feldsam@gmail.com>

--- config.sample.js
+++ config.sample.js
@@ -2,5 +2,6 @@ module.exports = {
     address: 'http://opennebula:2633/RPC2',
     user: 'oneadmin',
     token: 'someStrongPass',
-    backupDir: '/path/to/backup/dir/'
+    backupDir: '/path/to/backup/dir/',
+    bridgeList: ['node1', 'node2', 'node3']
 }
