9115e621a79dee99935fbf91d9142271c5b93709
Kristián Feldsam <feldsam@gmail.com>
2017-10-03 09:09:36 +0200

    UPdated shell script to check if prev command was success
    Signed-off-by: Kristián Feldsam <feldsam@gmail.com>

--- backup.sh
+++ backup.sh
@@ -5,6 +5,10 @@ BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo "Backup images"
 $BASEDIR/one-image-backup.js -vs
 
+if [ $? -ne 0 ]; then
+    exit 1
+fi
+
 echo "Backup deployments - system datastores without non-persistent disks"
 rsync -avhP --delete --exclude "disk.0.snap/0" oneadmin@node1.feldhost.cz:/var/lib/one/datastores/0/ /var/data/opennebula/0/
 rsync -avhP --delete --exclude "disk.0.snap/0" oneadmin@node1.feldhost.cz:/var/lib/one/datastores/110/ /var/data/opennebula/110/
